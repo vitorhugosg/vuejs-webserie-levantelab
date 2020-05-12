0028228c6b5500b57d94ebe4c0b68e6ae9d8f283
Vitor Hugo Soares Gonçalves <vitorhugosg@gmail.com>
2020-05-12 14:26:31 -0300

    Create metodos-e-eventos.vue

---
+++ metodos-e-eventos.vue
@@ -0,0 +1,16 @@
+<template>
+   <button @click="clickFunction">Clique Aqui.</button>
+</template>
+<script>
+//Exemplo de methodos e eventos
+export default {
+   methods:{
+      clickFunction(){
+         alert('fui clicado');
+      }
+   }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
